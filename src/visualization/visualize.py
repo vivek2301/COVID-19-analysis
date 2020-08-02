3a0be8a5f92c39acb2d260f62a6bb4c8421df96d
vivek yadav <raovivek777@gmail.com>
2020-08-03 01:09:30 +0200

    add bith dashboards into one

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -6,6 +6,7 @@ sys.path.append("..")
 from data.get_data import get_johns_hopkins
 from data.process_JH_data import store_relational_JH_data
 from features.build_features import build_JH_features
+from models.sir_model import sir_modelling
 
 import dash
 dash.__version__
@@ -21,7 +22,8 @@ print(os.getcwd())
 df_input_large=pd.read_csv('../../data/processed/COVID_final_set.csv',sep=';')
 
 
-fig = go.Figure()
+fig1 = go.Figure()
+fig2 = go.Figure()
 
 app = dash.Dash()
 app.layout = html.Div([
@@ -36,45 +38,66 @@ app.layout = html.Div([
 
     '''),
 
-    dcc.Markdown('''
-    ## Multi-Select Country for visualization
-    '''),
+    dcc.Tabs(id='tabs', value='visualization_tab', children=[
+        dcc.Tab(label='Visualization', value='visualization_tab', children=[
+
+            dcc.Markdown('''
+            ## Multi-Select Country for visualization
+            '''),
 
 
-    dcc.Dropdown(
-        id='country_drop_down',
-        options=[ {'label': each,'value':each} for each in df_input_large['country'].unique()],
-        value=['US', 'Germany','Italy'], # which are pre-selected
-        multi=True
-    ),
+            dcc.Dropdown(
+                id='country_list_drop_down',
+                options=[ {'label': each,'value':each} for each in df_input_large['country'].unique()],
+                value=['US', 'Germany','Italy'], # which are pre-selected
+                multi=True
+            ),
 
-    dcc.Markdown('''
-        ## Select Timeline of confirmed COVID-19 cases or the approximated doubling time
-        '''),
+            dcc.Markdown('''
+                ## Select Timeline of confirmed COVID-19 cases or the approximated doubling time
+                '''),
 
 
-    dcc.Dropdown(
-    id='doubling_time',
-    options=[
-        {'label': 'Timeline Confirmed ', 'value': 'confirmed'},
-        {'label': 'Timeline Confirmed Filtered', 'value': 'confirmed_filtered'},
-        {'label': 'Timeline Doubling Rate', 'value': 'confirmed_DR'},
-        {'label': 'Timeline Doubling Rate Filtered', 'value': 'confirmed_filtered_DR'},
-    ],
-    value='confirmed',
-    multi=False
-    ),
+            dcc.Dropdown(
+            id='doubling_time',
+            options=[
+                {'label': 'Timeline Confirmed ', 'value': 'confirmed'},
+                {'label': 'Timeline Confirmed Filtered', 'value': 'confirmed_filtered'},
+                {'label': 'Timeline Doubling Rate', 'value': 'confirmed_DR'},
+                {'label': 'Timeline Doubling Rate Filtered', 'value': 'confirmed_filtered_DR'},
+            ],
+            value='confirmed',
+            multi=False
+            ),
 
-    dcc.Graph(figure=fig, id='main_window_slope')
+            dcc.Graph(figure=fig1, id='visualization_graph')
+        ]),
+
+        dcc.Tab(label='SIR modelling', value='SIR_tab', children=[
+            dcc.Markdown('''
+            ## Select Country for SIR modelling
+            '''),
+
+
+            dcc.Dropdown(
+                id='country_drop_down',
+                options=[ {'label': each,'value':each} for each in df_input_large['country'].unique()],
+                value='Germany', # Pre-selected country
+                multi=False
+            ),
+
+            dcc.Graph(figure=fig2, id='SIR_graph')
+        ])
+    ])
 ])
 
 
 
 @app.callback(
-    Output('main_window_slope', 'figure'),
-    [Input('country_drop_down', 'value'),
+    Output('visualization_graph', 'figure'),
+    [Input('country_list_drop_down', 'value'),
     Input('doubling_time', 'value')])
-def update_figure(country_list,show_doubling):
+def update_visualization(country_list,show_doubling):
 
     if 'doubling_rate' in show_doubling:
         my_yaxis={'type':"log",
@@ -116,6 +139,54 @@ def update_figure(country_list,show_doubling):
                       },
 
                 yaxis=my_yaxis
+        )
+    }
+
+@app.callback(
+    Output('SIR_graph', 'figure'),
+    [Input('country_drop_down', 'value')])
+def update_sir(country):
+
+    traces = []
+    
+    df_plot=df_input_large[df_input_large['country']==country]
+    df_plot=df_plot[['state','country','confirmed','date']].groupby(['country','date']).agg(np.sum).reset_index()
+    df_plot.sort_values('date',ascending=True).head()
+    df_plot = df_plot.confirmed[35:]
+
+    t, fitted = sir_modelling(df_plot)
+
+    traces.append(dict(x=t,
+                        y=fitted,
+                        mode='markers',
+                        opacity=0.9,
+                        name='SIR fit'
+                    )
+            )
+    
+    traces.append(dict(x=t,
+                        y=df_plot,
+                        mode='lines',
+                        opacity=0.9,
+                        name='True data'
+                    )
+            )
+
+    return {
+            'data': traces,
+            'layout': dict (
+                width=1280,
+                height=720,
+                title='Fit of SIR model',
+
+                xaxis={'title':'Days',
+                        'tickangle':-45,
+                        'nticks':20,
+                        'tickfont':dict(size=14,color="#7f7f7f"),
+                      },
+
+                yaxis={'title':"Population infected"
+                      }
         )
     }
 
